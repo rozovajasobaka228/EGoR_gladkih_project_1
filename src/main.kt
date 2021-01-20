b5a2cfd082099ce4802e85c46fa2aa22b5f144ec
rozovajasobaka228 <egogladkih446@gmail.com>
2021-01-20 10:52:23 +0700

    Решили задачу №7

--- src/main.kt
+++ src/main.kt
@@ -1,4 +1,17 @@
 fun main()
 {
 
+var pi =3.14
+    print("Введите радиус: ")
+    var r = readLine()!!.toInt()
+    var L = 2*pi*r
+    var S = pi*r*r
+print("Диаметр окружности равен: $L\n длинна равна:  $S")
+
+
+
+
+
+
+
 }
